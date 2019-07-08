b670d70a383553492a6e65ba40973f3558a79aa5
h8z <hatesoul@mail.ru>
2019-07-08 05:09:13 +0300

    Реализован переход из неактивного(начального) состояния страницы в активное, передача коорд. главного маркера в инпут адреса

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ var PIN_X_MIN = 50;
 var PIN_X_MAX = 1150;
 var PIN_Y_MIN = 130;
 var PIN_Y_MAX = 630;
+var PIN_MAIN_WIDTH = 62;
+var PIN_MAIN_HEIGHT = 84;
 
 /**
  * Возвращает случайный элемент массива
@@ -61,12 +63,61 @@ var getSimilarAds = function (count) {
   return similarAds;
 };
 
+/**
+ * Перебирает DOM коллекцию, где каждому элементу удаляет или добавляет атрибут 'disabled' в зависимости от 2-ого параметра
+ * @param {domCollection} domCollection передаваемая коллекция
+ * @param {string} act действие, которое нужно совершить: 'enable' или 'disable'
+ */
+var enableOrDisableFormFields = function (domCollection, act) {
+  for (var i = 0; i < domCollection.length; i++) {
+    if (act === 'enable') {
+      domCollection[i].removeAttribute('disabled');
+    } else if (act === 'disable') {
+      domCollection[i].setAttribute('disabled', '');
+    }
+  }
+};
+
 var map = document.querySelector('.map');
 var pinList = map.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin')
     .content
     .querySelector('.map__pin');
 var pinFragment = document.createDocumentFragment();
+
+var pinMain = map.querySelector('.map__pin--main');
+var adForm = document.querySelector('.ad-form');
+var adFormFields = adForm.querySelectorAll('fieldset');
+var adFormAddress = adForm.querySelector('#address');
+var filtersForm = document.querySelector('.map__filters');
+var filtersFormFields = filtersForm.querySelectorAll('select, fieldset');
+
+/**
+ * Возвращает объект с координатами главного пина
+ * @param {Boolean} isCenter true - координаты равны середине пина; false || отсутствие параметра - место, куда метка указывает своим острым концом
+ * @return {object}
+ */
+var getPinMainCoordinates = function (isCenter) {
+  if (isCenter) {
+    return {
+      x: pinMain.offsetLeft + PIN_MAIN_WIDTH / 2,
+      y: pinMain.offsetTop + PIN_MAIN_WIDTH / 2
+    };
+  } else {
+    return {
+      x: pinMain.offsetLeft + PIN_MAIN_WIDTH / 2,
+      y: pinMain.offsetTop + PIN_MAIN_HEIGHT
+    };
+  }
+};
+
+/**
+ * Записывает переданные в виде объекта координаты в соотв. инпут
+ * @param {object} coordinates
+ */
+var setAdFormAddressCoordinates = function (coordinates) {
+  adFormAddress.value = coordinates.x + ', ' + coordinates.y;
+};
 
 getSimilarAds(OFFER_TOTAL).forEach(function (item) {
   var currentPin = item;
@@ -79,6 +130,27 @@ getSimilarAds(OFFER_TOTAL).forEach(function (item) {
   pinFragment.appendChild(pinElement);
 });
 
-pinList.appendChild(pinFragment);
+/**
+ * Приводит страницу в "активное состояние". Блок карты и формы становятся доступными. На карте появляются "похожие" объявления (моки)
+ */
+var onPinMainClick = function () {
+  map.classList.remove('map--faded');
+  adForm.classList.remove('ad-form--disabled');
+  enableOrDisableFormFields(adFormFields, 'enable');
+  enableOrDisableFormFields(filtersFormFields, 'enable');
+  pinList.appendChild(pinFragment);
+  setAdFormAddressCoordinates(getPinMainCoordinates());
+};
 
-map.classList.remove('map--faded');
+pinMain.addEventListener('click', onPinMainClick);
+
+/**
+ * Делает все формы страницы неактивными, записывает начальные координаты главного пина, равные ее центру
+ */
+var disablePage = function () {
+  setAdFormAddressCoordinates(getPinMainCoordinates(true));
+  enableOrDisableFormFields(adFormFields, 'disable');
+  enableOrDisableFormFields(filtersFormFields, 'disable');
+};
+
+disablePage();
